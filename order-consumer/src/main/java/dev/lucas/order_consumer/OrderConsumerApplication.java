f8e64a286ca437f993eef3adade88550fa1c8405
Lucas <lucaskaminskith@gmail.com>
2026-03-20 15:22:09 -0300

    Project created

---
+++ order-consumer/src/main/java/dev/lucas/order_consumer/OrderConsumerApplication.java
@@ -0,0 +1,13 @@
+package dev.lucas.order_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderConsumerApplication.class, args);
+	}
+
+}
---
+++ order-producer/src/main/java/dev/lucas/order_producer/OrderProducerApplication.java
@@ -0,0 +1,13 @@
+package dev.lucas.order_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderProducerApplication.class, args);
+	}
+
+}
